8be18f6e3d9c715a6bd092be01ff280f46fd7b4b
vincenthanguk <vincentkreusel@gmail.com>
2021-06-14 14:48:20 +0200

    added signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
-const userController = require('./../controllers/userController');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
