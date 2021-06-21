43f52f1069a77c9f9d465f5c387a938963489c7f
vincenthanguk <vincentkreusel@gmail.com>
2021-06-21 16:01:42 +0200

    POST and GET all reviews (review model, controller, router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -71,6 +72,7 @@ app.use((req, res, next) => {
 // mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // handling unhandled routes
 app.all('*', (req, res, next) => {
