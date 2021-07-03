08e2ab345dcbd24ae430d332ecf17fd9abd848d6
vincenthanguk <vincentkreusel@gmail.com>
2021-07-03 11:50:35 +0200

    calculate average 2

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -69,17 +69,43 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
   ]);
   console.log(stats);
 
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 reviewSchema.post('save', function () {
   // this points to current review
-
   this.constructor.calcAverageRatings(this.tour);
 });
+
+// findByIdAndUpdate
+// findByIdAndUpdate
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // passing data from pre middleware to post middleware (post middleware does not have access to query)
+  this.r = await this.findOne();
+  console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work her, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
+// alternate / cleaner way:
+// reviewSchema.post(/^findOneAnd/, async function (doc, next) {
+//   await doc.constructor.calcAverageRating(doc.tour);
+//   next();
+// });
 
 const Review = mongoose.model('Review', reviewSchema);
 
