23c22454c8af45eef2393420a0c7d27be3dfd99b
vincenthanguk <vincentkreusel@gmail.com>
2021-07-03 12:07:45 +0200

    prevent duplicate review, rounded average with setter function

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,6 +37,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// one user gets one review per tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: 'tour',
--- models/tourModel.js
+++ models/tourModel.js
@@ -39,6 +39,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'A rating must be above 1.0'],
       max: [5, 'A rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
