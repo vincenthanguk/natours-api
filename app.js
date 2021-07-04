827ec4ad7ae39cbe840fe4b93f2edbfa5e039e91
vincenthanguk <vincentkreusel@gmail.com>
2021-07-04 12:11:27 +0200

    first steps with pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,15 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
+
 // 1) GLOBAL MIDDLEWARES
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+// all static assets will automatically be loaded from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set Security HTTP Headers
 app.use(helmet());
 
@@ -55,9 +64,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -68,6 +74,13 @@ app.use((req, res, next) => {
 // 2) ROUTE HANDLERS
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Vincent',
+  });
+});
+
 // these routers are actually middleware
 // mounting the routers
 app.use('/api/v1/tours', tourRouter);
